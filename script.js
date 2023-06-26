2a862ab8fffd1ede6c311625f60c57011f9db6ee
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-06-26 14:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 //your JS code here. If required.
 const container = document.getElementsByClassName("container")[0];
 
-        function show(){
-            container.innerHTML = '';
-            const div = document.createElement("div");
-            div.id = "line";
-            container.append(div);
-        }
-
         function rotate(){
             const line = document.getElementById("line");
             line.className = "line"
             // container.append(div)
-        }
-
-        function stop(){
-            show();
         }
